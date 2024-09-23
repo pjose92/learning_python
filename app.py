41ec91f44737d2967e66a130609c90e9cbc4f14c
Jose Perez Guerrero <pjose14.jp38@yahoo.com>
2024-09-22 21:16:42 -0500

    strings and numbers

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-testing
+greetings = 'hello there!'
+age = 30
+first_name = 'JP'
+last_name = 'Perez'
+print(greetings.upper()) 
+print(greetings.islower()) 
+print(len(greetings))
+print(age)
+print(first_name)
+print(last_name)
+
+def add_numbers(a, b):
+    return a + b
+
+result = add_numbers(3, 5)
+print("The sum is:", result)
+print(max(3, 4, 18, 2, 5, 19))
+print(min(3, 4, 18, 1, 30, 20))
+
